94f0be0746a89e4f73e9c4630471d442acfbac66
FytoAmbiado <cristofher.ambiado@valoralabs.com>
2020-02-25 15:56:41 -0300

    creacion de AccountController

---
+++ MySqlLogin/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySqlLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            return this.View();
+        }
+
+    }
+}
