bae16a3b6f798d88af2d101a1ada7fe4ac3e50c2
FytoAmbiado <cristofher.ambiado@valoralabs.com>
2020-02-25 18:02:39 -0300

    formulario de registro listo

--- MySqlLogin/Controllers/AccountController.cs
+++ MySqlLogin/Controllers/AccountController.cs
@@ -48,6 +48,82 @@ namespace MySqlLogin.Controllers
             return this.View(model);
         }
 
+        public IActionResult Register()
+        {            
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterNewUserViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var user = await this.userHelper.GetUserByEmailAsync(model.UsuarioEntidad);
+
+                //NO EXISTE EL USUARIO EN LA BASE DE DATOS
+                if (user == null)
+                {
+                    try
+                    {                     
+
+                        user = new Entidad
+                        {
+                            RutEntidad = model.RutEntidad,
+                            UsuarioEntidad = model.UsuarioEntidad,
+                            EntEmail = model.EntEmail,
+                            EntTipo = model.EntTipo,
+
+
+
+                            //Nombre = model.FirstName,
+                            //Apellido = model.LastName,
+                            //Email = model.Username,
+                            //UserName = model.Username,
+                            //Address = model.Address,
+                            //PhoneNumber = model.PhoneNumber,
+                            //CityId = model.CityId,
+                            //City = city
+                        };
+
+                    //    var result = await this.userHelper.AddUserAsync(user, model.Password);
+                    //    if (result != IdentityResult.Success)
+                    //    {
+                    //        this.ModelState.AddModelError(string.Empty, "The user couldn't be created.");
+                    //        return this.View(model);
+                    //    }
+
+                        //    var myToken = await this.userHelper.GenerateEmailConfirmationTokenAsync(user);
+                        //    var tokenLink = this.Url.Action("ConfirmEmail", "Account", new
+                        //    {
+                        //        userid = user.Id,
+                        //        token = myToken
+                        //    }, protocol: HttpContext.Request.Scheme);
+
+
+                        //    this.mailHelper.SendMail(model.Username, "Email de confirmación", $"<h1>Email de confirmación</h1>" +
+                        //       $"Para habilitar el usuario, " + $"haga click en este link: </br></br><a href = \"{tokenLink}\">Confirmar Email</a>");
+
+                        //    this.ViewBag.Message = "Las instrucciones para registrar el usuario se enviaron a su correo.";
+
+                        //    return this.View(model);
+                    }
+                    catch (Exception Error)
+                    {
+                        this.ModelState.AddModelError(string.Empty, "No se pudo registrar al usuario, intente más tarde.");
+
+                        //this.ModelState.AddModelError(string.Empty, Error.Message);
+                    }
+
+                }
+
+                //USUARIO YA EXISTE EN LA BASE DE DATOS
+                this.ModelState.AddModelError(string.Empty, "El usuario que se intenta registrar ya esta creado en el sistema.");
+            }
+
+            return this.View(model);
+        }
+
+
         public async Task<IActionResult> Logout()
         {
             await this.userHelper.LogoutAsync();
--- MySqlLogin/Models/Entidad.cs
+++ MySqlLogin/Models/Entidad.cs
@@ -8,16 +8,25 @@ namespace MySqlLogin.Models
 {
     public class Entidad : IdentityUser
     {
-        //public int Id { get; set; }
+       
         public string RutEntidad { get; set; }
+
         public string ClaveEntidad { get; set; }
+
         public string UsuarioEntidad { get; set; }
+
         public string EntSalt { get; set; }
+
         public string EntENC { get; set; }
+
         public bool EntActivando { get; set; }
+
         public bool EntRestableciendo { get; set; }
+
         public string EntEmail { get; set; }
+
         public int EntTipo { get; set; }
+
         public string EntReferer { get; set; }
     }
 }
